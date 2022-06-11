f64fae9215d1e6de81bf46f3c0ce0d7c5aaadcaf
Tomás R. Pita <tomasrpita@gmail.com>
2022-06-11 12:03:03 +0200

    Cambios en nombres de etiquetas

--- app/LIB/print_labels.py
+++ app/LIB/print_labels.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from app.LIB.printers import printer_job
+# from app.LIB.printers import printer_job
 
 
 class PrinterLabels():
@@ -15,13 +15,13 @@ class PrinterLabels():
        self.sku = formdata['sku']
        self.tsc_label = formdata['tscLabel'] if formdata['tscLabel'] != 'ninguna' else ""
        self.zd_label = formdata['zdLabel'] if formdata['zdLabel'] != 'ninguna' else ""
-       
+
 
 
     def print_sample_label(self):
         printer = 'Impresora 1'
 
-        f=open("./printer_labels/sample_label.prn", "rb")
+        f=open("./printer_labels/new_sample_label.prn", "rb")
         s=f.read()
         f.close()
 
@@ -37,8 +37,8 @@ class PrinterLabels():
 
         #copies number
         s=s.replace(b'PRINT 1,1', bytes(f'PRINT {self.copies_mumber },1', 'utf-8'))
-        
-        printer_job(printer, s)
+
+        # printer_job(printer, s)
 
 
     def print_box_label(self):
@@ -51,7 +51,7 @@ class PrinterLabels():
 
         # name
         s=s.replace(b'DIVAIN-XXX', bytes(self.sku, 'utf-8'))
-    
+
         #barcode
         ean_13 = self.ean_13[:-1] + '>6' + self.ean_13[-1:]
         s=s.replace(b'123456789012>63', bytes(ean_13, 'utf-8'))
@@ -59,7 +59,7 @@ class PrinterLabels():
         #copies number
         s=s.replace(b'^PQ1,0,1,Y', bytes(f'^PQ{self.copies_mumber },0,1,Y', 'utf-8'))
 
-        printer_job(printer, s)
+        # printer_job(printer, s)
 
 
     def print_bottle_label(self):
@@ -71,17 +71,17 @@ class PrinterLabels():
 
         #sex
         s=s.replace(b'X X X X X', bytes(self.sex, 'utf-8'))
-    
+
         #lote
-        s=s.replace(b'L: XXXX', bytes(f'L: {self.lote}', 'utf-8'))
-        
+        s=s.replace(b'XXXX', bytes(f'L: {self.lote}', 'utf-8'))
+
         #numero
-        s=s.replace(b'ZZZ', bytes(self.sku.replace('DIVAIN-', ''), 'utf-8'))
+        s=s.replace(b'ZZZZZ', bytes(self.sku.replace('DIVAIN-', ''), 'utf-8'))
 
         #copies number
         s=s.replace(b'PRINT 1,1', bytes(f'PRINT {self.copies_mumber },1', 'utf-8'))
 
-        printer_job(printer, s)
+        # printer_job(printer, s)
 
     def print(self):
 
