4d21ed489e759311a3223434f3118d4a5a6b8533
Tomás R. Pita <tomasrpita@gmail.com>
2022-11-19 14:35:20 +0100

    Añade funcionalidad de recarga de bbdd desde google sheets

--- app/API/api.py
+++ app/API/api.py
@@ -1,15 +1,9 @@
 # coding: utf-8
 
 from flask_restful import Resource
-from flask import jsonify
-
 from app.LIB.reference_controller import get_reference_data
 
-class ApiInicio(Resource):
-    "WS de bienvenida (ejemplo)"
 
-    def get(self):
-        return jsonify({"Saludo": "Api funcionado"})
 
 
 class ApiReference(Resource):
--- app/web/routes.py
+++ app/web/routes.py
@@ -1,25 +1,30 @@
 # coding: utf-8
 
 import os
-from app.LIB.print_labels import PrintManager, PrinterLabels, ReferenceLabelData
-from flask import render_template, flash, request, redirect, url_for, abort, current_app
+from app.LIB.print_labels import PrinterLabels
+from flask import render_template, flash, request, abort, current_app
 from app.LIB.utils import get_database_name
-
-from app.LIB.printers import printer_job
-
-
 from app.LIB.utils import get_copies_number
-
 from app.web import bp
-
-log = current_app.logger
-
-database_name = get_database_name()
-
 
 def fake_printer_job(printer_name, printer_file):
 	log.info(f"Fake printer job printer name: {printer_name}")
 	log.info(f"Fake printer job printer_file: {printer_file}")
+
+
+# check if os is windows or mac
+if os.name == "nt":
+	# windows
+	from app.LIB.printers import printer_job
+	printers = printer_job
+else:
+	# mac
+	printers = fake_printer_job
+
+
+log = current_app.logger
+
+database_name = get_database_name()
 
 
 def database_exists(name):
@@ -35,8 +40,7 @@ def home():
 
 	if request.method == "POST":
 		formdata = request.form.to_dict(flat=True)
-		# PrinterLabels(formdata, fake_printer_job).print()
-		PrinterLabels(formdata, printer_job).print()
+		PrinterLabels(formdata, printers).print()
 
 		copies_number = request.form.get("CopiesNumber")
 
