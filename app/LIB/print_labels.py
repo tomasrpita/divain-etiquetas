b3804765eb14d6b8488bf6e45d9c1f8598b1f79f
Tomás R. Pita <tomasrpita@gmail.com>
2023-04-14 19:04:46 +0200

    Test for print barcode with MX Label

--- app/LIB/print_labels.py
+++ app/LIB/print_labels.py
@@ -361,17 +361,19 @@ class PrinterLabels:
             s = f.read()
             f.close()
 
-            # Lote bottle
-            s = s.replace(b"XXXXXX", bytes(f"{self.lote}", "utf-8"))
+            # # Lote bottle
+            # s = s.replace(b"XXXXXX", bytes(f"{self.lote}", "utf-8"))
 
-            # Ean box
-            s = s.replace(b"EEEEEEEEEEE", bytes(f"{self.ean_botes}", "utf-8"))
+            # # Ean box
+            # s = s.replace(b"EEEEEEEEEEE", bytes(f"{self.ean_botes}", "utf-8"))
 
-            # SKU box
-            s = s.replace(b"SSSSSSSSSSS", bytes(f"{self.sku}", "utf-8"))
+            # # SKU box
+            # s = s.replace(b"SSSSSSSSSSS", bytes(f"{self.sku}", "utf-8"))
 
-            # Lote box
-            s = s.replace(b"LLLLLLLLLLL", bytes(f"{self.lote}", "utf-8"))
+            # # Lote box
+            # s = s.replace(b"LLLLLLLLLLL", bytes(f"{self.lote}", "utf-8"))
+            # bar code
+            s = s.replace(b"123456789012!1003", bytes(f"{self.ean_botes}!1003", "utf-8"))
 
             # Copies number
             s = s.replace(b"PRINT 1,1", bytes(f"PRINT {self.copies_mumber },1", "utf-8"))
