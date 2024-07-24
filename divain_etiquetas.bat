5b8e2f0eb5692a84c9401e9769ddddfb8969d590
tomasrpita <tomasrpita@gmail.com>
2024-07-24 11:27:48 +0200

    cambios para nuevas fragancias home desde estación 01

--- divain_etiquetas.bat
+++ divain_etiquetas.bat
@@ -1,4 +1,4 @@
-cd /d "C:\Users\ENVASADO-04\divain-etiquetas\venv\Scripts"
+cd /d "C:\Users\Etiquetado_01\divain-etiquetas\.venv\Scripts"
 set FLASK_APP=..\..\app.py
-flask run -h localhost -p 5666
+flask run -h localhost -p 5200
 pause
